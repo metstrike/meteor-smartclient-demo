77289df476c9b5801cf1a4993c20657eeaacb5de
metstrike <metstrike.com@gmail.com>
2015-12-18 19:20:33 -0800

    Updated package.js

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'metstrike:meteor-smartclient-demo',
   version: '10.1.0',
   // Brief, one-line summary of the package.
-  summary: 'https://github.com/metstrike/meteor-smartclient-demo.git',
+  summary: 'Demo of Smartclient library integrated with Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/metstrike/meteor-smartclient-demo.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
